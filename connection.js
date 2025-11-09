ebb9825e450a052cc17b4d5776d0659ba5b4dc11
Abdulahi338 <cabdulahii723@gmail.com>
2025-11-09 17:29:04 +0300

    inserted Many students and using collection.insertMany

--- connection.js
+++ connection.js
@@ -17,8 +17,17 @@ async function insert(){
         console.log('Connected to MongoDB');
         const db = client.db('university');
         const collection = db.collection('students');
-        const result = await collection.insertOne({name:'Abdullahi Ibrahim', department:'Computer Science', address: 'Mogadishu'});
-        console.log('Insertion result:', result.insertedId);
+        // const result = await collection.insertOne({name:'Ahmed Ibrahim', department:'BA', address: 'Mogadishu', year:3});
+
+        //inset mony
+
+        const insertMony = await collection.insertMany([
+            {name:'Sara Ali', department:'CS', address: 'Hargeisa', year:2},
+            {name:'Omar Hassan', department:'IT', address: 'Bosaso', year:1},
+            {name:'Laila Yusuf', department:'ENG', address: 'Kismayo', year:4}
+        ]);
+        console.log('Insertion result:', insertMony.insertedIds);
+        // console.log('Insertion result:', result.insertedId);
 
     }catch(err){
         console.error('Insertion error:', err);
